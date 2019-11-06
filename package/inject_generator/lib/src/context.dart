e496bffd2b091196fb4609dd67c879b291627f59
Joel Trottier-Hebert <iamquebecois@gmail.com>
2019-11-06 09:43:05 -0500

    inject.dart: temporarily fixes issue with inject.dart's dependency of analyzer. Will likely break again soon when json_serializable updates to analyzer 0.39.x. At that point, inject.dart will also update, and get rid of deprecated APIs.

--- package/inject_generator/lib/src/context.dart
+++ package/inject_generator/lib/src/context.dart
@@ -7,12 +7,8 @@
 //                See also: http://cl/219513934
 
 import 'dart:async';
+
 import 'package:analyzer/dart/analysis/results.dart';
-
-// <TRANSITIONAL_API>
-import 'package:analyzer/src/dart/analysis/results.dart';
-// </TRANSITIONAL_API>
-
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:build/build.dart' as build show log;
@@ -46,7 +42,7 @@ BuilderContext get builderContext {
   if (context == null) {
     throw new StateError(
       'No current $BuilderContext is active. Start your build function using '
-          '"runInContext" to be able to use "builderContext"',
+      '"runInContext" to be able to use "builderContext"',
     );
   }
   return context;
@@ -103,7 +99,8 @@ class BuilderLogger {
     // <TRANSITIONAL_API>
     ElementDeclarationResult elementDeclaration;
     if (element.kind != ElementKind.DYNAMIC) {
-      var parsedLibrary = ParsedLibraryResultImpl.tmp(element.library);
+      var parsedLibrary =
+          element.session.getParsedLibraryByElement(element.library);
       if (parsedLibrary.state == ResultState.VALID) {
         elementDeclaration = parsedLibrary.getElementDeclaration(element);
       }
